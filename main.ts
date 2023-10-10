87d56aa0eec009d168252c87c9761697ce0b16b9
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-10-10 15:06:55 +0200

    maak lijn volgen

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
 basic.forever(function () {
-	
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(21, 21)
+    }
+})
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(21, 11)
+    }
+})
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(11, 21)
+    }
 })
